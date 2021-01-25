74536abde48419757ea8e9467575aa3c17a2b14f
Benjamin Bulis <beni1999@gmx.at>
2021-01-25 22:57:05 +0100

    Added Response Body file for findByDate

---
+++ src/main/java/com/bulis/temp/backend/helper/FindTemperatureByDateRequestBody.java
@@ -0,0 +1,20 @@
+package com.bulis.temp.backend.helper;
+
+/**
+ * File needed for findByDate Request
+ *
+ * @author Benjamin Bulis
+ * @version V1.0
+ */
+public class FindTemperatureByDateRequestBody {
+
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
